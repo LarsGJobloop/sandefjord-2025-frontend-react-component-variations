ddc76c0507d8030e37bc606424336f1cde0ac170
zabronax <zabronax@gmail.com>
2025-01-14 12:28:32 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import style from "./App.module.css";
+
+function App() {
+  return (
+    <div>
+      <header className={style.header}>
+        <h1>React Component Variations</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
