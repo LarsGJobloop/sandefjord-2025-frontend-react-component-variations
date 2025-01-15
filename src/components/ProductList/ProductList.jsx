9744812347ebbf0a7ed8c25a6a0d1b3f3f2a8625
zabronax <zabronax@gmail.com>
2025-01-15 14:02:14 +0100

    add conditonal rendering example

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,19 +1,57 @@
 import { products } from './products'
 
 import style from './ProductList.module.css'
+import { useState } from 'react'
 
 export function ProductList() {
   return (
     <ul className={style.productList}>
       {products.map((product) => {
         return (
-          <li key={product.id} className={style.productCard}>
-            <p>{product.name}</p>
-            <p>{product.price}</p>
-            <img src={product.image} alt={product.description} />
+          <li key={product.id}>
+            <ProductCard product={product} />
           </li>
         )
       })}
     </ul>
+  )
+}
+
+function useFavorite(id) {
+  const [isProcessing, setProcessing] = useState()
+
+  function favorite() {
+    console.log("Favoriting product: " + id)
+    setProcessing(true)
+    setTimeout(() => setProcessing(false), 1000)
+  }
+
+  function unfavorite() {
+    console.log("Unfavoriting product: " + id)
+    setProcessing(true)
+    setTimeout(() => setProcessing(false), 1000)
+  }
+
+  return {
+    isProcessing,
+    favorite,
+    unfavorite
+  }
+}
+
+function ProductCard({product}) {
+  const {favorite, isProcessing, unfavorite} = useFavorite(product.id)
+
+  return (
+    <div className={style.productCard}>
+      <p>{product.name}</p>
+      <p>{product.price}</p>
+      <img src={product.image} alt={product.description} />
+      {
+        product.favorited
+          ? <button disabled={isProcessing} onClick={unfavorite} >Unfavorite</button>
+          : <button disabled={isProcessing} onClick={favorite} >Favorite</button>
+      }
+    </div>
   )
 }
--- src/components/ProductList/products.js
+++ src/components/ProductList/products.js
@@ -4,13 +4,15 @@ export const products = [
     name: "Xbox X",
     price: 3999,
     image: "https://images.unsplash.com/photo-1591799264318-7e6ef8ddb7ea?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    description: "The latest game console from Microsoft"
+    description: "The latest game console from Microsoft",
+    favorited: false
   },
   {
     id: 1,
     name: "Playstation 5",
     price: 5999,
     image: "https://images.unsplash.com/photo-1591799264318-7e6ef8ddb7ea?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    description: "The latest game console from Sony"
+    description: "The latest game console from Sony",
+    favorited: true
   }
 ]
